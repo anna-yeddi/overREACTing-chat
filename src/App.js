843c12621a8fd7bda44c06432d532d1045482340
Anna Kova <m3ta007@gmail.com>
2020-06-10 01:05:56 -0400

    Dynamic chat log is created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import MessageHistory from './components/MessageHistory'
+import log from './data/chat-log.json'
 
 function App() {
+  const messages = log
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="clearfix container">
+      <div className="chat">
+        <div className="chat-history">
+          <MessageHistory list={messages} />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
